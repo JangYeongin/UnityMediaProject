6edd3cd85cfd6b80101707a689fbb30214c934e2
Jang <donblue@naver.com>
2015-08-25 14:23:59 +0900

    By J

---
+++ Assets/Script/DrawGizmo.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DrawGizmo : MonoBehaviour {
+
+	void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(transform.position, 1);
+    }
+}
